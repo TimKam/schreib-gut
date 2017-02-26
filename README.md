96960f81ee7bc83b634bc2ba3444a997491ca5a9
Timotheus Kampik <timotheus.kampik@signavio.com>
2017-02-26 12:14:28 +0100

    setup basic write-good extension for German language

---
+++ README.md
@@ -0,0 +1,3 @@
+`schreib-gut` is a German language extension for the [write-good](linter).
+It's right now under development. You can't integrate it with any released
+version of `write-good`, yet.
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  weasel: {
+    fn: require('weasel-words-german'),
+    explanation: 'is a weasel word'
+  },
+  tooWordy: {
+    fn: require('too-wordy-german'),
+    explanation: 'is wordy or unneeded'
+  }
+}
